a109d13740291d9239d70d63ba43189fec103b5b
Maneesh Johnson <mnsh.exe@gmail.com>
2025-09-29 13:05:56 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const message: string = "(Webpack Typescript Template) index.ts: Hello World";
+console.log(message);
+
+const div: HTMLDivElement = document.createElement("div");
+div.innerHTML = message;
+document.body.appendChild(div);
