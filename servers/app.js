a4464c9caceeb505597089ef46b08bce7d544de3
Maneesh <mnsh.exe@gmail.com>
2025-10-04 16:55:05 +0530

    added release endpoint to app server

--- servers/app.js
+++ servers/app.js
@@ -10,8 +10,11 @@ app.use("/", serveIndex("examples", { icons: true }));
 app.use("/devBuild", express.static(".temp/devBuild/texscript"));
 app.use("/devBuild", serveIndex(".temp/devBuild/texscript", { icons: true }));
 
+app.use("/releases", express.static("releases"));
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
   console.log(`./examples -> /`);
   console.log(`./.temp/devBuild/texscript -> /devBuild`);
+  console.log(`./releases -> /releases`);
 });
