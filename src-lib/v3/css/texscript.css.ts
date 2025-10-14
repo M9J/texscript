02be1cfa088444c78ee3023420127dfc0867a5a4
Maneesh <mnsh.exe@gmail.com>
2025-10-14 14:26:07 +0530

    misc style fixes

--- src-lib/v3/css/texscript.css.ts
+++ src-lib/v3/css/texscript.css.ts
@@ -2,8 +2,12 @@ import BASE from "./native/base.css";
 
 import PRINT from "./media/print.css";
 
+import TEXSCRIPT_BLOCKS from "./custom/texscript-Blocks.css";
+
 import TEXSCRIPT_CORE from "./custom/texscript-core.css";
 
 import TEXSCRIPT_LIST from "./custom/texscript-List.css";
 
-export default BASE + PRINT + TEXSCRIPT_CORE + TEXSCRIPT_LIST;
+import TEXSCRIPT_LINE from "./custom/texscript-Line.css";
+
+export default BASE + PRINT + TEXSCRIPT_BLOCKS + TEXSCRIPT_CORE + TEXSCRIPT_LIST + TEXSCRIPT_LINE;
